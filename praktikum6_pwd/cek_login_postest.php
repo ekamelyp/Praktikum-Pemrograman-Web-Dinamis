d5cce58392b6237ec3d7c6684e0e1ac7aefe649b
Eka Meliyani Putri <62529413+ekamelyp@users.noreply.github.com>
2021-11-27 17:59:41 +0700

    Postest praktikum 6 PWD
    File postest

---
+++ praktikum6_pwd/cek_login_postest.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+include "../praktikum3_pwd/koneksi.php";
+
+$id_user = $_POST['id_user'];
+$pass = md5($_POST['password']);
+
+$sql = "SELECT * FROM users WHERE id_user='$id_user' AND password='$pass'";
+if ($_POST["captcha_code"] == $_SESSION["captcha_code"]) {
+
+    $login = mysqli_query($conn, $sql);
+    $ketemu = mysqli_num_rows($login);
+    $r = mysqli_fetch_array($login);
+
+    if ($ketemu > 0) {
+        $_SESSION['iduser'] = $r['id_user'];
+        $_SESSION['passuser'] = $r['password'];
+        $_SESSION['ipkuser'] = $r['ipk'];
+
+        echo "USER BERHASIL LOGIN<br>";
+        echo "id user =", $_SESSION['iduser'], "<br>";
+        echo "password=", $_SESSION['passuser'], "<br>";
+        echo "IPK=", $_SESSION['ipkuser'], "<br>";
+        echo "<a href=logout.php><b>LOGOUT</b></a></center>";
+    } else {
+        echo "<center>Login gagal! username & password tidak benar<br>";
+        echo "<a href=form_login.php><b>ULANGILAGI</b></a></center>";
+    }
+    mysqli_close($conn);
+} else {
+    echo "<center>Login gagal! Captcha tidak sesuai!<br>";
+    echo "<a href=form_login.php><b>ULANGI LAGI</b></a></center>";
+}
